85a54b1ef0c4f8eacc20e978b3927cf199cb7cd7
Vlad Iftime <vlad.iftime@solarracinggroningen.nl>
2024-01-10 18:15:33 +0100

    Add purge and content options to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+
   content: ['./src/**/*.{html,svelte,js,ts}'],
   theme: {
     extend: {},
